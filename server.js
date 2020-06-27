9681d3312438149914d317edb54820188a03b09e
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-06-27 14:52:59 -0400

    added the routes with the help of the starwars activity we did last week

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
-// Dependencies
-const express= require('express');
-const path= require('path')
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
-// Configure the express server
-const port= 8080
-// This will create the server for us
-const app= express();
-// Processes data parsing
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+var express = require("express");
 
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
 
-// --------------------- Shows link in terminal -----------------------------
+// Tells node that we are creating an "express" server
+var app = express();
 
-// Listen to open up the link to your page
-app.listen(port, function() {
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
 
-    console.log(`Server is listening at http://localhost:${port}`)
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-})
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on http://localhost:" + PORT);
+});
