2a55495442178cec47dd1ca1b2517879227eece9
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-06-28 09:17:42 -0400

    added a way for the server to convert the string numbers that are input by the user into numbers to calculate difference

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendData= require('../data/friends')
+var friendData= require('../data/friends');
 
 // ===============================================================================
 // ROUTING
@@ -30,11 +30,53 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.post('/api/friends', function(req, res) {
+
+    let newFriend= req.body
+
+    // This for loop will turn the responses that are taken from the survey
+    // into actual numbers instead of strings for us to use to calculate the 
+    // difference in choices to match the friends up
+    for (let i = 0; i < newFriend.scores.length; i++) {
+
+      if (newFriend.scores[i] == '1 (Strongly Disagree') {
+
+        newFriend.scores[i]= 1
+
+      } else if (newFriend.scores[i] == '5 (Strongly Agree') {
+
+        newFriend.scores[i]= 5
+
+      } else {
+
+        newFriend.scores[i]= parseInt(newFriend.scores[i])
+
+      }
+
+    }
+
+    // This for loop is going to loop over the friends that are in the friend.js file
+    // as well as do the math to choose the best friend that will match witht the users
+    // input
+    for (let i = 0; i < friendData.length; i++) {
+
+      let friendCompared= friendData[i]
+      let totalDifference
+
+      for (let j = 0; j < friendCompared.scores.length; k++) {
+
+
+        
+      }
+
+    }
+
+    
     // Note the code here. Our 'server' will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value 'true' have a table
     // req.body is available since we're using the body parsing middleware
     friendData.push(req.body)
     res.json(true)
+
   });
 
 };
